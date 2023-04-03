e8f4c0ed314284c659ba4c25d493ba1c9367c957
kartikeya47 <kartikeya.rawat2020@vitstudent.ac.in>
2023-04-03 11:49:38 +0530

    Added edge cases

--- includes/prisoner_out.inc.php
+++ includes/prisoner_out.inc.php
@@ -8,10 +8,14 @@
             exit();
         }else{
           $sql1="UPDATE Prisoner SET Status_inout='OUT' WHERE Prisoner_id='$prisoner_id' ";
-          if(!mysqli_query($conn,$sql1)){
+          $sql2="SELECT * FROM Prisoner WHERE Prisoner_id='$prisoner_id' ";
+          $result=mysqli_query($conn,$sql2);
+          $resultCheck=mysqli_num_rows($result);
+          if($resultCheck === 0){
             header("Location: ../prisoner_out.php?error=sqlerror");
             exit();
         }else{
+            mysqli_query($conn,$sql1);
             header("Location: ../prisoner_out.php?error=success");
               exit();
         }
